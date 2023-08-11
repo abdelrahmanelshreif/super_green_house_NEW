84e52ec48e8f97bdaabe4dcf966052d9dacc595a
Naglaa Saeed <naglaasaeedalgzar@gmail.com>
2023-08-12 00:44:56 +0300

    login_api

---
+++ lib/model/LoginModel.dart
@@ -0,0 +1,50 @@
+class LoginModel {
+  String? message;
+  Data? data;
+  bool? status;
+
+  LoginModel({
+    this.status,
+    this.message,
+    this.data,
+  });
+
+  LoginModel.fromJson(dynamic json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+}
+
+class Data {
+  num? id;
+  String? name;
+  String? email;
+  String? phone;
+  String? image;
+  num? points;
+  num? credit;
+  String? token;
+
+  Data({
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+    this.image,
+    this.points,
+    this.credit,
+    this.token,
+  });
+
+  Data.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    image = json['image'];
+    points = json['points'];
+    credit = json['credit'];
+    token = json['token'];
+  }
+}
